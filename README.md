462cc726c381b40940ad7b6c1d6279829e609edb
Kevin Malakoff <kmalakoff@gmail.com>
2020-04-06 21:22:06 -0700

    Added next with error argument. Added compatibility tests.

--- README.md
+++ README.md
@@ -8,12 +8,12 @@ const maximize = require('maximize-iterator');
 (async ()=> {
   // run 1024 in parallel until done - promises
   var iterator = // create it somehow with a next method returing {done: value: }
-  await maximize(iterator, { concurrency: 1024, each: (value) => { /* do something */ } });
+  await maximize(iterator, { concurrency: 1024, each: (err, value) => { /* do something including false stop */ } });
 })();
 
 // run 1024 in parallel until done - callbacks
 var iterator = // create it somehow with a next method returing {done: value: }
-maximize(iterator, { concurrency: 1024, each: (value) => { /* do something */ } }, (err) => {
+maximize(iterator, { concurrency: 1024, each: (err, value) => { /* do something including false stop */ } }, (err) => {
   /* done */
 });
 ```
--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 var callOnce = require('./lib/callOnce');
-var callIteratorsCallback = require('./lib/callIteratorsCallback');
-
-const HAS_ASYNC_ITERATOR = typeof Symbol !== 'undefined' && Symbol.asyncIterator;
-var useAsyncIterator = require('./lib/useAsyncIterator');
-var callIteratorsAsyncIterator = HAS_ASYNC_ITERATOR ? require('./lib/callIteratorsAsyncIterator') : null;
+var callIteratorNext = require('./lib/callIteratorNext');
+var maximizeNext = require('./lib/maximizeNext');
 
 var DEFAULT_CONCURRENCY = 4096;
 var MAXIMUM_BATCH = 10;
@@ -13,21 +10,22 @@ module.exports = function maximizeIterator(iterator, options, callback) {
     callback = options;
     options = {};
   }
-  options = options || {};
-  options = {
-    concurrency: options.concurrency || DEFAULT_CONCURRENCY,
-    batch: options.batch || MAXIMUM_BATCH,
-    each: options.each,
-    counter: 0,
-  };
 
   if (typeof callback === 'function') {
-    if (useAsyncIterator(iterator)) return callIteratorsAsyncIterator(iterator, options, callOnce(callback));
-    else return callIteratorsCallback(iterator, options, callOnce(callback));
-  }
-  return new Promise(function (resolve, reject) {
-    maximizeIterator(iterator, options, function (err) {
-      err ? reject(err) : resolve();
+    options = options || {};
+    options = {
+      concurrency: options.concurrency || DEFAULT_CONCURRENCY,
+      batch: options.batch || MAXIMUM_BATCH,
+      each: options.each,
+      counter: 0,
+    };
+
+    maximizeNext(callIteratorNext(iterator), options, callOnce(callback));
+  } else {
+    return new Promise(function (resolve, reject) {
+      maximizeIterator(iterator, options, function (err) {
+        err ? reject(err) : resolve();
+      });
     });
-  });
+  }
 };
