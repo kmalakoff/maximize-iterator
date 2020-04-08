4848f005208703c94f7372c5f9ed21ff0f55aeb4
Kevin Malakoff <kmalakoff@gmail.com>
2020-04-07 19:44:41 -0700

    Move to external libraries

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var callOnce = require('./lib/callOnce');
-var callIteratorNext = require('./lib/callIteratorNext');
+var nextCallback = require('iterator-next-callback');
+var callOnce = require('call-once-next-tick');
+
 var maximizeNext = require('./lib/maximizeNext');
 
 var DEFAULT_CONCURRENCY = 4096;
-var MAXIMUM_BATCH = 10;
+var MAXIMUM_BATCH = 100;
 
 module.exports = function maximizeIterator(iterator, options, callback) {
   if (typeof options === 'function') {
@@ -16,11 +17,11 @@ module.exports = function maximizeIterator(iterator, options, callback) {
     options = {
       concurrency: options.concurrency || DEFAULT_CONCURRENCY,
       batch: options.batch || MAXIMUM_BATCH,
-      each: options.each || function (err, value) {},
+      each: options.each || function () {},
       counter: 0,
     };
 
-    maximizeNext(callIteratorNext(iterator), options, callOnce(callback));
+    maximizeNext(nextCallback(iterator), options, callOnce(callback));
   } else {
     return new Promise(function (resolve, reject) {
       maximizeIterator(iterator, options, function (err) {
