9f60d92e42022714be00ec5131a223c4c8186817
Kevin Malakoff <kmalakoff@gmail.com>
2020-04-11 12:46:27 -0700

    Added limit tests

--- test/spec/promises.test.js
+++ test/spec/promises.test.js
@@ -175,4 +175,217 @@ describe('promises interface', function () {
         done();
       });
   });
+
+  it('limit 1 (concurrency default)', function (done) {
+    var iterator = new Iterator([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+    var results = [];
+    maximizeIterator(
+      iterator,
+      function (value) {
+        results.push(value);
+      },
+      {
+        limit: 1,
+      }
+    )
+      .then(function () {
+        assert.equal(iterator.values.length, 9);
+        assert.deepEqual(results, [1]);
+        done();
+      })
+      .catch(function (err) {
+        assert.ok(!err);
+      });
+  });
+
+  it('limit 1 (concurrency 1)', function (done) {
+    var iterator = new Iterator([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+    var results = [];
+    maximizeIterator(
+      iterator,
+      function (value) {
+        results.push(value);
+      },
+      {
+        concurrency: 1,
+        limit: 1,
+      }
+    )
+      .then(function () {
+        assert.equal(iterator.values.length, 9);
+        assert.deepEqual(results, [1]);
+        done();
+      })
+      .catch(function (err) {
+        assert.ok(!err);
+      });
+  });
+
+  it('limit 1 (concurrency 10)', function (done) {
+    var iterator = new Iterator([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+    var results = [];
+    maximizeIterator(
+      iterator,
+      function (value) {
+        results.push(value);
+      },
+      {
+        concurrency: 10,
+        limit: 1,
+      }
+    )
+      .then(function () {
+        assert.equal(iterator.values.length, 9);
+        assert.deepEqual(results, [1]);
+        done();
+      })
+      .catch(function (err) {
+        assert.ok(!err);
+      });
+  });
+
+  it('limit 5 (concurrency default)', function (done) {
+    var iterator = new Iterator([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+    var results = [];
+    maximizeIterator(
+      iterator,
+      function (value) {
+        results.push(value);
+      },
+      {
+        limit: 5,
+      }
+    )
+      .then(function () {
+        assert.equal(iterator.values.length, 5);
+        assert.deepEqual(results, [1, 2, 3, 4, 5]);
+        done();
+      })
+      .catch(function (err) {
+        assert.ok(!err);
+      });
+  });
+
+  it('limit 5 (concurrency 1)', function (done) {
+    var iterator = new Iterator([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+    var results = [];
+    maximizeIterator(
+      iterator,
+      function (value) {
+        results.push(value);
+      },
+      {
+        concurrency: 1,
+        limit: 5,
+      }
+    )
+      .then(function () {
+        assert.equal(iterator.values.length, 5);
+        assert.deepEqual(results, [1, 2, 3, 4, 5]);
+        done();
+      })
+      .catch(function (err) {
+        assert.ok(!err);
+      });
+  });
+
+  it('limit 5 (concurrency 10)', function (done) {
+    var iterator = new Iterator([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+    var results = [];
+    maximizeIterator(
+      iterator,
+      function (value) {
+        results.push(value);
+      },
+      {
+        concurrency: 10,
+        limit: 5,
+      }
+    )
+      .then(function () {
+        assert.equal(iterator.values.length, 5);
+        assert.deepEqual(results, [1, 2, 3, 4, 5]);
+        done();
+      })
+      .catch(function (err) {
+        assert.ok(!err);
+      });
+  });
+
+  it('limit 20 (concurrency default)', function (done) {
+    var iterator = new Iterator([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+    var results = [];
+    maximizeIterator(
+      iterator,
+      function (value) {
+        results.push(value);
+      },
+      {
+        limit: 20,
+      }
+    )
+      .then(function () {
+        assert.equal(iterator.values.length, 0);
+        assert.deepEqual(results, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+        done();
+      })
+      .catch(function (err) {
+        assert.ok(!err);
+      });
+  });
+
+  it('limit 20 (concurrency 1)', function (done) {
+    var iterator = new Iterator([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+    var results = [];
+    maximizeIterator(
+      iterator,
+      function (value) {
+        results.push(value);
+      },
+      {
+        concurrency: 1,
+        limit: 20,
+      }
+    )
+      .then(function () {
+        assert.equal(iterator.values.length, 0);
+        assert.deepEqual(results, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+        done();
+      })
+      .catch(function (err) {
+        assert.ok(!err);
+      });
+  });
+
+  it('limit 20 (concurrency 10)', function (done) {
+    var iterator = new Iterator([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+    var results = [];
+    maximizeIterator(
+      iterator,
+      function (value) {
+        results.push(value);
+      },
+      {
+        concurrency: 10,
+        limit: 20,
+      }
+    )
+      .then(function () {
+        assert.equal(iterator.values.length, 0);
+        assert.deepEqual(results, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+        done();
+      })
+      .catch(function (err) {
+        assert.ok(!err);
+      });
+  });
 });
