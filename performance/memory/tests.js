3d7596d0a66010bf5e3f8e3d45ed6e1729341257
Kevin Malakoff <kmalakoff@gmail.com>
2020-04-20 17:50:35 -0700

    Update formatting

--- performance/memory/tests.js
+++ performance/memory/tests.js
@@ -11,25 +11,24 @@ Iterator.prototype.next = function next(callback) {
 };
 
 module.exports = async function run({ maximize, version, testOptions }) {
-  var suite = new BenchmarkSuite('maximize', 'Memory');
+  var suite = new BenchmarkSuite('maximize ' + version, 'Memory');
 
   for (const test of testOptions) {
-    suite.add(`${version}-${test.name}`, async function (fn) {
+    suite.add(`${test.name}`, async function (fn) {
       const iterator = new Iterator(1000);
       await maximize(iterator, fn, test.options);
     });
   }
 
   suite.on('cycle', (results) => {
-    for (var key in results) console.log(`${results[key].name} (${key}) x ${suite.formatStats(results[key].stats)}`);
+    for (var key in results) console.log(`${results[key].name.padStart(8, ' ')}| ${suite.formatStats(results[key].stats)} - ${key}`);
   });
   suite.on('complete', function (results) {
     console.log('-----Largest-----');
-    for (var key in results) console.log(`${results[key].name} (${key}) x ${suite.formatStats(results[key].stats)}`);
+    for (var key in results) console.log(`${results[key].name.padStart(8, ' ')}| ${suite.formatStats(results[key].stats)} - ${key}`);
   });
 
-  console.log('---------------');
-  console.log('Running ' + suite.name);
-  await suite.run({ time: 1000 }); //, heapdumpTrigger: 1024 * 20 });
+  console.log('----------' + suite.name + '----------');
+  await suite.run({ time: 1000 }); //, heapdumpTrigger: 1024 * 100 });
   console.log('');
 };
