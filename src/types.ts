935ce7c320145a58f835b2a8521b23408eee3f98
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-04 07:04:03 -0700

    Comprehensive types

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,20 @@
+export type { CallbackIterator } from 'iterator-next-callback';
 export type MaximizeCallback = (err?: Error) => void;
 export interface MaximizeOptions {
   async?: boolean;
   concurrency?: number;
   limit?: number;
   callbacks?: boolean;
+  batch?: number;
   error?: (err: Error) => void;
+}
+
+export interface MaximizeOptionsPrivate<T> extends MaximizeOptions {
+  each: EachFunction<T>;
+  stop: (counter: number) => boolean;
+  total: number;
+  counter: number;
+  done: boolean;
 }
 
 export type EachFunctionCallback<T> = (value: T, callback?: (err?: Error, stop?: boolean) => void) => void;
