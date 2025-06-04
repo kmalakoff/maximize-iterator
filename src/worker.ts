7a5433756d407efa50e49b11aa120d32753c03d8
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-04 07:04:03 -0700

    Comprehensive types

--- src/worker.ts
+++ src/worker.ts
@@ -1,31 +1,31 @@
 import nextCallback from 'iterator-next-callback';
 import createProcessor from './createProcessor.js';
 
+import type { EachFunction, MaximizeCallback, MaximizeOptions, MaximizeOptionsPrivate } from './types.js';
+
 const DEFAULT_CONCURRENCY = 4096;
 const DEFAULT_LIMIT = Infinity;
 const MAXIMUM_BATCH = 10;
 
-export default function worker(iterator, each, options, callback) {
-  options = {
+export default function worker<T>(iterator: AsyncIterableIterator<T>, each: EachFunction<T>, options_: MaximizeOptions, callback: MaximizeCallback) {
+  let options: MaximizeOptionsPrivate<T> = {
+    callbacks: options_.callbacks || options_.async,
+    concurrency: options_.concurrency || DEFAULT_CONCURRENCY,
+    limit: options_.limit || DEFAULT_LIMIT,
+    batch: options_.batch || MAXIMUM_BATCH,
+    // default is exit on error
+    error: options_.error || (() => true),
     each,
-    callbacks: options.callbacks || options.async,
-    concurrency: options.concurrency || DEFAULT_CONCURRENCY,
-    limit: options.limit || DEFAULT_LIMIT,
-    batch: options.batch || MAXIMUM_BATCH,
-    error:
-      options.error ||
-      (() => {
-        return true; // default is exit on error
-      }),
+    stop: (counter) => counter > options.batch,
     total: 0,
     counter: 0,
-    stop: (counter) => counter > options.batch,
+    done: false,
   };
 
-  let processor = createProcessor(nextCallback(iterator), options, (err) => {
+  let processor = createProcessor(nextCallback(iterator), options, (err?: Error) => {
     options = null;
     processor = null;
-    return callback(err);
+    callback(err);
   });
   processor();
 }
