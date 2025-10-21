dd8ffe2835ac76afaf317d557f9a30b6924adbae
Kevin Malakoff <kmalakoff@gmail.com>
2025-10-20 21:29:43 -0700

    Upgrade modules

--- test/exports/import.test.ts
+++ test/exports/import.test.ts
@@ -1,5 +1,4 @@
 import assert from 'assert';
-// @ts-ignore
 import maximizeIterator, { createProcessor } from 'maximize-iterator';
 
 describe('exports .ts', () => {
--- test/unit/AsyncIterator.ts
+++ test/unit/AsyncIterator.ts
@@ -1,5 +1,4 @@
 import assert from 'assert';
-// @ts-ignore
 import maximizeIterator from 'maximize-iterator';
 import Pinkie from 'pinkie-promise';
 
