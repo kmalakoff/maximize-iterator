72ee4719ffd8143c0d44825a47f770ecba3cd6ed
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-17 12:27:20 -0800

    Refactor for flush loop pattern

--- src/createProcessor.ts
+++ src/createProcessor.ts
@@ -5,11 +5,9 @@ import type { EachDoneCallback, Next, Processor, ProcessorOptions } from './type
 const isError = (err?: Error): boolean => err && err.stack !== undefined && err.message !== undefined;
 
 function processDone<T>(err: Error, options: ProcessorOptions<T>, callback: EachDoneCallback) {
-  // mark this iteration done
   options.err = options.err || err;
   options.done = true;
 
-  // process done
   if (!options.done || options.counter > 0) return false;
   callback(options.err, options.done);
   return true;
@@ -18,30 +16,33 @@ function processDone<T>(err: Error, options: ProcessorOptions<T>, callback: Each
 function processResult(err, keep, options, callback) {
   options.counter--;
 
-  // mark this iteration done
   if ((err && compat.defaultValue(options.error(err), false)) || (!err && !compat.defaultValue(keep, true))) {
     options.err = options.err || err;
     options.done = true;
   }
 
-  // process done
   if (!options.done || options.counter > 0) return false;
   callback(options.err, options.done);
   return true;
 }
 
 export default function createProcessor<T, TReturn = unknown>(next: Next<T>, options: ProcessorOptions<T>, callback: EachDoneCallback): Processor {
-  let isProcessing = false;
-  return function processor(doneOrError?: Error | boolean): undefined {
-    const error = doneOrError as Error;
-    if (doneOrError && processDone(isError(error) ? error : null, options, callback)) return;
-    if (isProcessing) return;
-    isProcessing = true;
+  let flushing = false;
+
+  function callDefer(err?: Error, keep?: boolean): undefined {
+    const shouldContinue = !processResult(err, keep, options, callback);
+    if (flushing) return;
+    if (shouldContinue) flush();
+  }
+
+  function flush(): undefined {
+    flushing = true;
 
     while (options.counter < options.concurrency) {
       if (options.done || !options.canProcess()) break;
       if (options.total >= options.limit) {
         processDone(null, options, callback);
+        flushing = false;
         return;
       }
       options.total++;
@@ -49,12 +50,18 @@ export default function createProcessor<T, TReturn = unknown>(next: Next<T>, opt
 
       next((err?: Error, result?: IteratorResult<T, TReturn>) => {
         if (err || result.done) {
-          return !processResult(err, false, options, callback) && !isProcessing ? processor() : undefined;
+          return callDefer(err, false);
         }
-        compat.asyncFunction(options.each, options.callbacks, result.value, (err, keep) => (!processResult(err, keep, options, callback) && !isProcessing ? processor() : undefined));
+        compat.asyncFunction(options.each, options.callbacks, result.value, (err, keep) => callDefer(err, keep));
       });
     }
 
-    isProcessing = false;
+    flushing = false;
+  }
+
+  return function processor(doneOrError?: Error | boolean): undefined {
+    const error = doneOrError as Error;
+    if (doneOrError && processDone(isError(error) ? error : null, options, callback)) return;
+    flush();
   };
 }
