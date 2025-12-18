d0fedccdb2792f54e8206bf3ff7619816282a76e
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-17 18:18:31 -0800

    Fix void types

--- src/maximizeIterator.ts
+++ src/maximizeIterator.ts
@@ -1,21 +1,21 @@
 import type { Callback, EachFunction, ForEachOptions, Iterator } from './types.ts';
 import worker from './worker.ts';
 
-export default function maximizeIterator<T, TReturn = unknown, TNext = unknown>(iterator: AsyncIterator<T, TReturn, TNext>, each: EachFunction<T>, callback: Callback): undefined;
-export default function maximizeIterator<T, TReturn = unknown, TNext = unknown>(iterator: AsyncIterator<T, TReturn, TNext>, each: EachFunction<T>, options: ForEachOptions, callback: Callback): undefined;
-export default function maximizeIterator<T, TReturn = unknown, TNext = unknown>(iterator: AsyncIterable<T, TReturn, TNext>, each: EachFunction<T>, callback: Callback): undefined;
-export default function maximizeIterator<T, TReturn = unknown, TNext = unknown>(iterator: AsyncIterable<T, TReturn, TNext>, each: EachFunction<T>, options: ForEachOptions, callback: Callback): undefined;
-export default function maximizeIterator<T, TReturn = unknown, TNext = unknown>(iterator: AsyncIterableIterator<T, TReturn, TNext>, each: EachFunction<T>, callback: Callback): undefined;
-export default function maximizeIterator<T, TReturn = unknown, TNext = unknown>(iterator: AsyncIterableIterator<T, TReturn, TNext>, each: EachFunction<T>, options: ForEachOptions, callback: Callback): undefined;
+export default function maximizeIterator<T, TReturn = unknown, TNext = unknown>(iterator: AsyncIterator<T, TReturn, TNext>, each: EachFunction<T>, callback: Callback): void;
+export default function maximizeIterator<T, TReturn = unknown, TNext = unknown>(iterator: AsyncIterator<T, TReturn, TNext>, each: EachFunction<T>, options: ForEachOptions, callback: Callback): void;
+export default function maximizeIterator<T, TReturn = unknown, TNext = unknown>(iterator: AsyncIterable<T, TReturn, TNext>, each: EachFunction<T>, callback: Callback): void;
+export default function maximizeIterator<T, TReturn = unknown, TNext = unknown>(iterator: AsyncIterable<T, TReturn, TNext>, each: EachFunction<T>, options: ForEachOptions, callback: Callback): void;
+export default function maximizeIterator<T, TReturn = unknown, TNext = unknown>(iterator: AsyncIterableIterator<T, TReturn, TNext>, each: EachFunction<T>, callback: Callback): void;
+export default function maximizeIterator<T, TReturn = unknown, TNext = unknown>(iterator: AsyncIterableIterator<T, TReturn, TNext>, each: EachFunction<T>, options: ForEachOptions, callback: Callback): void;
 
-export default function maximizeIterator<T, TReturn = unknown, TNext = unknown>(iterator: AsyncIterator<T, TReturn, TNext>, each: EachFunction<T>): Promise<undefined>;
-export default function maximizeIterator<T, TReturn = unknown, TNext = unknown>(iterator: AsyncIterator<T, TReturn, TNext>, each: EachFunction<T>, options: ForEachOptions): Promise<undefined>;
-export default function maximizeIterator<T, TReturn = unknown, TNext = unknown>(iterator: AsyncIterable<T, TReturn, TNext>, each: EachFunction<T>): Promise<undefined>;
-export default function maximizeIterator<T, TReturn = unknown, TNext = unknown>(iterator: AsyncIterable<T, TReturn, TNext>, each: EachFunction<T>, options: ForEachOptions): Promise<undefined>;
-export default function maximizeIterator<T, TReturn = unknown, TNext = unknown>(iterator: AsyncIterableIterator<T, TReturn, TNext>, each: EachFunction<T>): Promise<undefined>;
-export default function maximizeIterator<T, TReturn = unknown, TNext = unknown>(iterator: AsyncIterableIterator<T, TReturn, TNext>, each: EachFunction<T>, options: ForEachOptions): Promise<undefined>;
+export default function maximizeIterator<T, TReturn = unknown, TNext = unknown>(iterator: AsyncIterator<T, TReturn, TNext>, each: EachFunction<T>): Promise<void>;
+export default function maximizeIterator<T, TReturn = unknown, TNext = unknown>(iterator: AsyncIterator<T, TReturn, TNext>, each: EachFunction<T>, options: ForEachOptions): Promise<void>;
+export default function maximizeIterator<T, TReturn = unknown, TNext = unknown>(iterator: AsyncIterable<T, TReturn, TNext>, each: EachFunction<T>): Promise<void>;
+export default function maximizeIterator<T, TReturn = unknown, TNext = unknown>(iterator: AsyncIterable<T, TReturn, TNext>, each: EachFunction<T>, options: ForEachOptions): Promise<void>;
+export default function maximizeIterator<T, TReturn = unknown, TNext = unknown>(iterator: AsyncIterableIterator<T, TReturn, TNext>, each: EachFunction<T>): Promise<void>;
+export default function maximizeIterator<T, TReturn = unknown, TNext = unknown>(iterator: AsyncIterableIterator<T, TReturn, TNext>, each: EachFunction<T>, options: ForEachOptions): Promise<void>;
 
-export default function maximizeIterator<T, TReturn = unknown, TNext = unknown>(iterator: Iterator<T, TReturn, TNext>, each: EachFunction<T>, options?: ForEachOptions | Callback, callback?: Callback): undefined | Promise<undefined> {
+export default function maximizeIterator<T, TReturn = unknown, TNext = unknown>(iterator: Iterator<T, TReturn, TNext>, each: EachFunction<T>, options?: ForEachOptions | Callback, callback?: Callback): void | Promise<void> {
   if (typeof each !== 'function') throw new Error('Missing each function');
   if (typeof options === 'function') {
     callback = options as Callback;
@@ -23,6 +23,9 @@ export default function maximizeIterator<T, TReturn = unknown, TNext = unknown>(
   }
   options = options || {};
 
-  if (typeof callback === 'function') return worker(iterator, each, options, callback) as undefined;
-  return new Promise((resolve, reject) => worker(iterator, each, options, (err) => (err ? reject(err) : resolve(undefined))));
+  if (typeof callback === 'function') {
+    worker(iterator, each, options, callback);
+    return;
+  }
+  return new Promise((resolve, reject) => worker(iterator, each, options, (err) => (err ? reject(err) : resolve())));
 }
