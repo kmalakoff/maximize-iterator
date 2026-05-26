d4b92e0b11e23f7b9de877e27fe5d3363ee32fb9
Kevin Malakoff <kmalakoff@gmail.com>
2026-05-26 14:23:41 -0700

    Fix broken types #2

--- src/createProcessor.d.ts
+++
@@ -1,2 +0,0 @@
-import type { EachDoneCallback, Next, Processor, ProcessorOptions } from './types.ts';
-export default function createProcessor<T, _TReturn = unknown>(next: Next<T>, options: ProcessorOptions<T>, callback: EachDoneCallback): Processor;
