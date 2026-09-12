9a29b590e578094b145eb2a32073ab1936a7db29
Kevin Malakoff <kmalakoff@gmail.com>
2026-09-11 18:23:04 -0700

    Upgrade modules

--- test/unit/promises.test.ts
+++ test/unit/promises.test.ts
@@ -2,6 +2,9 @@ import assert from 'assert';
 import maximizeIterator from 'maximize-iterator';
 import Pinkie from 'pinkie-promise';
 
+// biome-ignore lint/suspicious/noShadowRestrictedNames: Legacy
+const Symbol: SymbolConstructor = typeof global.Symbol === 'undefined' ? ({ asyncIterator: '@@' } as unknown as SymbolConstructor) : global.Symbol;
+
 class Iterator<T> implements AsyncIterator<T> {
   values: T[];
 
@@ -12,6 +15,23 @@ class Iterator<T> implements AsyncIterator<T> {
     return new Pinkie<IteratorResult<T, null>>((resolve: (value: IteratorResult<T, null>) => void) => {
       return resolve(this.values.length ? { done: false, value: this.values.shift() as T } : { done: true, value: null });
     });
+  }
+}
+
+class RejectingIterator<T> implements AsyncIterableIterator<T> {
+  values: T[];
+
+  constructor(values: T[]) {
+    this.values = values;
+  }
+  next(): Promise<IteratorResult<T, null>> {
+    return new Pinkie<IteratorResult<T, null>>((resolve: (value: IteratorResult<T, null>) => void, reject: (reason?: unknown) => void) => {
+      if (this.values.length) this.values.shift();
+      return this.values.length > 0 ? reject(new Error('Failed')) : resolve({ done: true, value: null });
+    });
+  }
+  [Symbol.asyncIterator](): AsyncIterableIterator<T> {
+    return this;
   }
 }
 
@@ -55,6 +75,26 @@ describe('promises interface', () => {
       (err) => {
         if (err) return done(err);
         assert.equal(iterator.values.length, 0);
+        done();
+      }
+    );
+  });
+
+  it('routes iterator promise rejections to the error handler', (done) => {
+    const iterator = new RejectingIterator<number>([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+    const errors: Error[] = [];
+    maximizeIterator<number>(
+      iterator,
+      (_value: number): void => {},
+      {
+        error(err: Error): void {
+          errors.push(err);
+        },
+      },
+      (err) => {
+        if (err) return done(err);
+        assert.equal(errors.length, 9);
         done();
       }
     );
